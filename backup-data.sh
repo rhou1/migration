1954ababa1e8cdc5146af5d4b29f9a8e4b99f3b4
robert.hou@hpe.com <hour@m2-lr1-dev-vm211098.mip.storage.hpecorp.net>
2024-08-02 20:57:11 -0700

    handle notebook pvc put arguments in config file

--- backup-data.sh
+++ backup-data.sh
@@ -1,21 +1,23 @@
 #!/bin/bash
 
-set -x
+# set -x
 
-if [[ $# -ne 5 ]]; then
-  echo need chain-ca.pem file, AWS access key, AWS secret key, restic password, and deployment
-  exit 1
-fi
-
-CHAIN_CA_PEM=$1
-AWS_ACCESS_KEY_ID=$2
-AWS_SECRET_ACCESS_KEY=$3
-RESTIC_REPOSITORY=$4
-RESTIC_PASSWORD=$5
-
-# s3:https://m2-lr1-dev-vm212066.mip.storage.hpecorp.net:9000/newairflow
+. ./backup.config
 
 echo backup chain-ca.pem $CHAIN_CA_PEM AWS access key id $AWS_ACCESS_KEY_ID AWS secret access key $AWS_SECRET_ACCESS_KEY restic repository $RESTIC_REPOSITORY restic password $RESTIC_PASSWORD
+
+remove_backup_labels() {
+
+  nodes=$(kubectl get nodes | grep worker | awk '{print $1}')
+  for node in $nodes
+  do
+    labels=$(kubectl get node $node -o jsonpath='{.metadata.labels}' | jq '.' | grep '\"hpe.com/backup' | cut -d: -f 1 | sed -e 's/"//g')
+    for label in $labels
+    do
+      kubectl label nodes $node $label-
+    done
+  done
+}
 
 push_chain_ca_pem() {
 
@@ -66,9 +68,6 @@ push_chain_ca_pem() {
     echo $password | sshpass scp -o StrictHostKeyChecking=no $CHAIN_CA_PEM $username@$node:/tmp/chain-ca.pem
     echo $password | sshpass ssh -o StrictHostKeyChecking=no $username@$node sudo mv /tmp/chain-ca.pem /mnt
     echo $password | sshpass ssh -o StrictHostKeyChecking=no $username@$node sudo chmod 777 /mnt/chain-ca.pem
-    echo $password | sshpass scp -o StrictHostKeyChecking=no backupfiles $username@$node:/tmp/backupfiles
-    echo $password | sshpass ssh -o StrictHostKeyChecking=no $username@$node sudo mv /tmp/backupfiles /mnt
-    echo $password | sshpass ssh -o StrictHostKeyChecking=no $username@$node sudo chmod 777 /mnt/backupfiles
   done
 }
 
@@ -77,11 +76,21 @@ create_backup_pod() {
 	local namespace=$1
 	local pvc_name=$2
 
+        pv=$(kubectl get pvc $pvc_name -n $namespace | grep $pvc_name | awk '{print $3}')
+        # user-pvc can be mounted on more than one node.  Take the first one
+        node=$(kubectl get volumeattachments -n $namespace | grep $pv | awk '{print $4}' | head -1)
+        if [[ ! -z $node ]]; then
+          kubectl label nodes $node hpe.com/backup-$pvc_name=$pvc_name
+        else
+          kubectl label nodes $default_node hpe.com/backup-$pvc_name=$pvc_name
+        fi
+        logfile="/tmp/backup-$namespace-$pvc_name-$DEPLOYMENT"
+
 	cat <<EOF | kubectl apply -f -
 apiVersion: v1
 kind: Pod
 metadata:
-  name: backup-$pvc_name
+  name: hpe-backup-$pvc_name
   namespace: $namespace
   labels:
     app: backup
@@ -103,11 +112,18 @@ spec:
       mountPath: /data
     - name: awsconfig-mnt
       mountPath: /mnt
+    resources:
+      requests:
+        memory: 1Gi
+        cpu: 100m
+      limits:
+        memory: 1Gi
+        cpu: 300m
     command:
     - /bin/sh
     - -c
     - |
-      # cp -p -r /mnt/chain-ca.pem .
+      echo "start pod"
       if restic --cacert /mnt/chain-ca.pem cat config >/dev/null 2>&1; then
         echo already initialized
       else
@@ -115,14 +131,9 @@ spec:
         restic --cacert /mnt/chain-ca.pem init
       fi
       echo "init completed"
-      echo in pod, deployment $deployment
-      echo backupfiles
-      cat /mnt/backupfiles
-      restic --cacert /mnt/chain-ca.pem backup --verbose --tag "namespace=$namespace,pvc=$pvc_name,deployment=$deployment" --files-from /mnt/backupfiles
-      # restic --cacert /mnt/chain-ca.pem backup /data --verbose --tag "namespace=$namespace,pvc=$pvc_name,deployment=$deployment" --files-from /mnt/backupfiles
-      # restic --cacert /mnt/chain-ca.pem backup /data --verbose --tag "namespace=$namespace,pvc=$pvc_name,deployment=plat3"
+      restic --cacert /mnt/chain-ca.pem backup /data --verbose --tag "namespace=$namespace,pvc=$pvc_name,deployment=$deployment" | tee $logfile
       echo "Backup completed for $namespace/$pvc_name"
-      restic --cacert /mnt/chain-ca.pem snapshots
+      restic --cacert /mnt/chain-ca.pem snapshots | tee -a $logfile
       # Keep the pod running to allow for verification
       sleep infinity
   volumes:
@@ -135,7 +146,102 @@ spec:
     name: awsconfig-mnt
   restartPolicy: Never
   nodeSelector:
-    backup-$namespace: $namespace
+    hpe.com/backup-$pvc_name: $pvc_name
+EOF
+
+	echo "Created backup pod for $namespace:$pvc_name"
+}
+
+# Function to create a backup pod for a single PVC
+create_user_backup_pod() {
+	local namespace=$1
+	local pvc_name=$2
+
+        pv=$(kubectl get pvc $pvc_name -n $namespace | grep $pvc_name | awk '{print $3}')
+        # some pvc like user-pvc can be mounted on more than one node.  Take the first one
+        node=$(kubectl get volumeattachments -n $namespace | grep $pv | awk '{print $4}' | head -1)
+        if [[ ! -z $node ]]; then
+          kubectl label nodes $node hpe.com/backup-$pvc_name=$pvc_name
+        else
+          # pvc has not been assigned to a node.
+          # assign pvc to a default node, so duplicate requests are ignored
+          kubectl label nodes $default_node hpe.com/backup-$pvc_name=$pvc_name
+        fi
+
+        # remove random characters at the end of the namespace
+        # because there will be different characters on the target system
+        # remove random characters at the end of the pvc if the pvc begins with "volume"
+        restic_namespace=$(echo $namespace | sed -e 's/\([a-z0-9-]*\)-[a-z0-9]*/\1/')
+        if [[ $pvc_name == "volume"* ]]; then
+          restic_pvc_name=$(echo $pvc_name | sed -e 's/\([a-z0-9-]*\)-[a-z0-9]*/\1/')
+        else
+          restic_pvc_name=$pvc_name
+        fi
+
+        logfile="/tmp/backup-$restic_namespace-$restic_pvc_name-$DEPLOYMENT"
+
+	cat <<EOF | kubectl apply -f -
+apiVersion: v1
+kind: Pod
+metadata:
+  name: hpe-backup-$pvc_name
+  namespace: $namespace
+  labels:
+    app: backup
+spec:
+  containers:
+  - name: backup-container
+    image: restic/restic:latest
+    env:
+    - name: AWS_ACCESS_KEY_ID
+      value: "$AWS_ACCESS_KEY_ID"
+    - name: AWS_SECRET_ACCESS_KEY
+      value: "$AWS_SECRET_ACCESS_KEY"
+    - name: RESTIC_REPOSITORY
+      value: $RESTIC_REPOSITORY
+    - name: RESTIC_PASSWORD
+      value: $RESTIC_PASSWORD
+    volumeMounts:
+    - name: data
+      mountPath: /data
+    - name: awsconfig-mnt
+      mountPath: /mnt
+    resources:
+      requests:
+        memory: 1Gi
+        cpu: 100m
+      limits:
+        memory: 1Gi
+        cpu: 300m
+    command:
+    - /bin/sh
+    - -c
+    - |
+      if restic --cacert /mnt/chain-ca.pem cat config >/dev/null 2>&1; then
+        echo already initialized
+      else
+        echo 'not initialized'
+        restic --cacert /mnt/chain-ca.pem init
+      fi
+      echo "init completed"
+      restic --cacert /mnt/chain-ca.pem backup /data --verbose --tag "namespace=$restic_namespace,pvc=$restic_pvc_name,deployment=$deployment" --exclude "/data/$restic_namespace/$restic_namespace-*"
+      echo restic rc $?
+      echo "Backup completed for $namespace/$pvc_name"
+      restic --cacert /mnt/chain-ca.pem snapshots
+      echo restic rc $?
+      # Keep the pod running to allow for verification
+      sleep infinity
+  volumes:
+  - name: data
+    persistentVolumeClaim:
+      claimName: $pvc_name
+  - hostPath:
+      path: /mnt
+      type: DirectoryOrCreate
+    name: awsconfig-mnt
+  restartPolicy: Never
+  nodeSelector:
+    hpe.com/backup-$pvc_name: $pvc_name
 EOF
 
 	echo "Created backup pod for $namespace:$pvc_name"
@@ -145,10 +251,20 @@ EOF
 pvc_list=$(kubectl get configmap pvc-list -o jsonpath='{.data.pvcs}')
 
 # Check if the ConfigMap exists and contains data
-if [ -z "$pvc_list" ]; then
-	echo "Error: ConfigMap 'pvc-list' not found or empty"
-	exit 1
+if [[ -z "$pvc_list" ]]; then
+	echo "ConfigMap 'pvc-list' not found or empty"
 fi
+
+# Get the user PVC list from the ConfigMap
+user_pvc_list=$(kubectl get configmap user-pvc-list -o jsonpath='{.data.pvcs}')
+
+# Check if the ConfigMap exists and contains data
+if [[ -z "$user_pvc_list" ]]; then
+	echo "ConfigMap 'user-pvc-list' not found or empty"
+fi
+
+remove_backup_labels
+default_node=$(kubectl get nodes | grep worker | head -1 | awk '{print $1}')
 
 # Get deployment name
 deployment=$(kubectl get cm ezua-cluster-config -n ezua-system -o jsonpath='{.data.cluster\.deploymentName}')
@@ -157,14 +273,26 @@ deployment=$(kubectl get cm ezua-cluster-config -n ezua-system -o jsonpath='{.da
 # push_chain_ca_pem
 
 # Process each PVC in the list
-echo pvc_list $pvc_list
-echo "$pvc_list" | while IFS=': ' read -r namespace pvc_info; do
-        echo namespace $namespace pvc_info $pvc_info
+if [[ ! -z "$pvc_list" ]]; then
+  echo "$pvc_list" | while IFS=': ' read -r namespace pvc_info; do
 	pvc_name=$(echo $pvc_info | cut -d' ' -f1)
 
 	# Create the backup pod
 	create_backup_pod "$namespace" "$pvc_name"
-done
+        sleep 10
+  done
+fi
+
+# Process each user PVC in the list
+if [[ ! -z "$user_pvc_list" ]]; then
+  echo "$user_pvc_list" | while IFS=': ' read -r namespace pvc_info; do
+	pvc_name=$(echo $pvc_info | cut -d' ' -f1)
+
+	# Create the backup pod
+	create_user_backup_pod "$namespace" "$pvc_name"
+        sleep 10
+  done
+fi
 
 echo "Backup pods creation process completed"
 
--- restore-job.sh
+++ restore-job.sh
@@ -2,20 +2,22 @@
 
 set -x
 
-if [[ $# -ne 7 ]]; then
-  echo need chain-ca.pem file, AWS access key, AWS secret key, restic password, deployment, and pvc
-  exit 1
-fi
+. ./restore.config
 
-CHAIN_CA_PEM=$1
-AWS_ACCESS_KEY_ID=$2
-AWS_SECRET_ACCESS_KEY=$3
-RESTIC_PASSWORD=$4
-DEPLOYMENT=$5
-NAMESPACE=$6
-PVC=$7
+echo restore chain-ca.pem $CHAIN_CA_PEM AWS access key id $AWS_ACCESS_KEY_ID AWS secret access key $AWS_SECRET_ACCESS_KEY restic password $RESTIC_PASSWORD deployment $DEPLOYMENT
 
-echo restore chain-ca.pem $CHAIN_CA_PEM AWS access key id $AWS_ACCESS_KEY_ID AWS secret access key $AWS_SECRET_ACCESS_KEY restic password $RESTIC_PASSWORD deployment $DEPLOYMENT namespace $NAMESPACE pvc $PVC
+remove_restore_labels() {
+
+  nodes=$(kubectl get nodes | grep worker | awk '{print $1}')
+  for node in $nodes
+  do
+    labels=$(kubectl get node $node -o jsonpath='{.metadata.labels}' | jq '.' | grep '\"restore' | cut -d: -f 1 | sed -e 's/"//g')
+    for label in $labels
+    do
+      kubectl label nodes $node $label-
+    done
+  done
+}
 
 push_chain_ca_pem() {
   if [[ $(rpm -qa | grep sshpass | wc -l) -ne 1 ]]; then
@@ -73,6 +75,16 @@ create_restore_pod() {
 	local namespace=$1
 	local pvc_name=$2
 
+        pv=$(kubectl get pvc $pvc_name -n $namespace | grep $pvc_name | awk '{print $3}')
+        # user-pvc can be mounted on more than one node.  Take the first one
+        node=$(kubectl get volumeattachments -n $namespace | grep $pv | awk '{print $4}' | head -1)
+        # echo node $node
+        if [[ ! -z $node ]]; then
+          kubectl label nodes $node restore-$pvc_name=$pvc_name
+        else
+          kubectl label nodes $default_node restore-$pvc_name=$pvc_name
+        fi
+
 	cat <<EOF | kubectl apply -f -
 apiVersion: v1
 kind: Pod
@@ -89,7 +101,7 @@ spec:
     - name: AWS_SECRET_ACCESS_KEY
       value: "$AWS_SECRET_ACCESS_KEY"
     - name: RESTIC_REPOSITORY
-      value: "s3:https://m2-lr1-dev-vm212066.mip.storage.hpecorp.net:9000/notebook1"
+      value: $RESTIC_REPOSITORY
     - name: RESTIC_PASSWORD
       value: "$RESTIC_PASSWORD"
     volumeMounts:
@@ -104,9 +116,7 @@ spec:
       restic --cacert /mnt/chain-ca.pem snapshots
       echo in pod, deployment $DEPLOYMENT
       echo host $host pvc $PVC
-      # restic --cacert /mnt/chain-ca.pem restore --tag "namespace=$namespace,pvc=$pvc_name,deployment=$DEPLOYMENT" latest --target / --path /data --host $host
-      # restic --cacert /mnt/chain-ca.pem restore --tag "namespace=$namespace,pvc=$PVC,deployment=$DEPLOYMENT" latest --target / --path /data --host backup-$PVC--include /hpedemo-user01/*.ipynb
-      restic --cacert /mnt/chain-ca.pem restore --tag "namespace=$NAMESPACE,pvc=$PVC,deployment=$DEPLOYMENT" latest --target / --host backup-$PVC
+      restic --cacert /mnt/chain-ca.pem restore --tag "namespace=$namespace,pvc=$pvc_name,deployment=$DEPLOYMENT" latest --target /
       echo "Restore completed for $namespace/$pvc_name"
       # Keep the pod running to allow for verification
       sleep infinity
@@ -119,6 +129,97 @@ spec:
       type: DirectoryOrCreate
     name: awsconfig-mnt
   restartPolicy: Never
+  nodeSelector:
+    restore-$pvc_name: $pvc_name
+EOF
+
+	echo "Created restore pod for $namespace:$pvc_name"
+}
+
+# Function to create a restore pod for a single PVC
+create_user_restore_pod() {
+	local namespace=$1
+	local pvc_name=$2
+
+        pv=$(kubectl get pvc $pvc_name -n $namespace | grep $pvc_name | awk '{print $3}')
+        # some pvc like user-pvc can be mounted on more than one node.  Take the first one
+        node=$(kubectl get volumeattachments -n $namespace | grep $pv | awk '{print $4}' | head -1)
+        # echo node $node
+        if [[ ! -z $node ]]; then
+          kubectl label nodes $node restore-$pvc_name=$pvc_name
+        else
+          # pvc has not been assigned to a node.
+          # assign pvc to a default node, so duplicate requests are ignored
+          kubectl label nodes $default_node restore-$pvc_name=$pvc_name
+        fi
+
+        # remove random characters at the end of the namespace
+        # because there are different characters on the source system
+        # remove random characters at the end of the pvc if the pvc begins with "volume"
+        restic_namespace=$(echo $namespace | sed -e 's/\([a-z0-9-]*\)-[a-z0-9]*/\1/')
+        if [[ $pvc_name == "volume"* ]]; then
+          restic_pvc_name=$(echo $pvc_name | sed -e 's/\([a-z0-9-]*\)-[a-z0-9]*/\1/')
+        else
+          restic_pvc_name=$pvc_name
+        fi
+        logfile="/tmp/restore-$restic_namespace-$restic_pvc_name-$DEPLOYMENT"
+
+
+	cat <<EOF | kubectl apply -f -
+apiVersion: v1
+kind: Pod
+metadata:
+  name: restore-$pvc_name
+  namespace: $namespace
+spec:
+  containers:
+  - name: restore-container
+    image: restic/restic:latest
+    env:
+    - name: AWS_ACCESS_KEY_ID
+      value: "$AWS_ACCESS_KEY_ID"
+    - name: AWS_SECRET_ACCESS_KEY
+      value: "$AWS_SECRET_ACCESS_KEY"
+    - name: RESTIC_REPOSITORY
+      value: $RESTIC_REPOSITORY
+    - name: RESTIC_PASSWORD
+      value: "$RESTIC_PASSWORD"
+    volumeMounts:
+    - name: data
+      mountPath: /data
+    - name: awsconfig-mnt
+      mountPath: /mnt
+    - name: logdir
+      mountPath: /tmp
+    command:
+    - /bin/sh
+    - -c
+    - |
+      restic version
+      echo restic_namespace $restic_namespace restic_pvc_name $restic_pvc_name
+      restic --cacert /mnt/chain-ca.pem snapshots | tee $logfile
+      echo rc $?
+      echo >> $logfile
+      restic --cacert /mnt/chain-ca.pem restore --tag "namespace=$restic_namespace,pvc=$restic_pvc_name,deployment=$DEPLOYMENT" latest --target / | tee -a $logfile
+      echo rc $?
+      echo "Restore completed for $namespace/$pvc_name"
+      # Keep the pod running to allow for verification
+      sleep infinity
+  volumes:
+  - name: data
+    persistentVolumeClaim:
+      claimName: $pvc_name
+  - hostPath:
+      path: /mnt
+      type: DirectoryOrCreate
+    name: awsconfig-mnt
+  - hostPath:
+      path: /tmp
+      type: DirectoryOrCreate
+    name: logdir
+  restartPolicy: Never
+  nodeSelector:
+    restore-$pvc_name: $pvc_name
 EOF
 
 	echo "Created restore pod for $namespace:$pvc_name"
@@ -130,21 +231,45 @@ pvc_list=$(kubectl get configmap pvc-list -o jsonpath='{.data.pvcs}')
 # Check if the ConfigMap exists and contains data
 if [ -z "$pvc_list" ]; then
 	echo "Error: ConfigMap 'pvc-list' not found or empty"
-	exit 1
+	# exit 1
 fi
+
+# Get the user PVC list from the ConfigMap
+user_pvc_list=$(kubectl get configmap user-pvc-list -o jsonpath='{.data.pvcs}')
+
+# Check if the ConfigMap exists and contains data
+if [[ -z "$user_pvc_list" ]]; then
+        echo "ConfigMap 'user-pvc-list' not found or empty"
+fi
+
+remove_restore_labels
+default_node=$(kubectl get nodes | grep worker | head -1 | awk '{print $1}')
 
 # temporarily comment out during testing
 # push_chain_ca_pem
 
 # Process each PVC in the list
-echo "$pvc_list" | while IFS=': ' read -r namespace pvc_info; do
+if [[ ! -z "$pvc_list" ]]; then
+  echo "$pvc_list" | while IFS=': ' read -r namespace pvc_info; do
+        echo namespace $namespace pvc_info $pvc_info
 	pvc_name=$(echo $pvc_info | cut -d' ' -f1)
-
         host="backup-$pvc_name"
-        echo host $host
 
 	# Create the restore pod
 	create_restore_pod "$namespace" "$pvc_name"
-done
+  done
+fi
+
+# Process each user PVC in the list
+if [[ ! -z "$user_pvc_list" ]]; then
+  # echo user_pvc_list $user_pvc_list
+  echo "$user_pvc_list" | while IFS=': ' read -r namespace pvc_info; do
+        pvc_name=$(echo $pvc_info | cut -d' ' -f1)
+
+        # Create the restore pod
+        create_user_restore_pod "$namespace" "$pvc_name"
+        sleep 10
+  done
+fi
 
 echo "Restore pods creation process completed"
